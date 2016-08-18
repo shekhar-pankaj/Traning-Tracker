8aab0a41ce8a57c981b46428a89edfac756988ea
shekhar-pankaj <shekharpankaj@outlook.com>
2016-08-18 12:26:41 +0530

    git ignore added

--- TrainingTracker.Common/Entity/FeedbackType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TrainingTracker.Common.Entity
-{
-    public class FeedbackType
-    {
-        public int FeedbackTypeId { get; set; }
-        public string Description { get; set; }
-    }
-}
